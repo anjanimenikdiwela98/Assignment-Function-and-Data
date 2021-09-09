1cd7c83989162251884706293c11042b67dd6e8f
anjanimenikdiwela98 <62288674+anjanimenikdiwela98@users.noreply.github.com>
2021-09-09 09:48:35 +0530

    Update Q4.scala

--- Q4.scala
+++ Q4.scala
@@ -11,12 +11,12 @@ object Q4
 	{
 		println("*******Overdraft----Total Balance----Interest*********")
 
-		var acc1=new account("S123",1001,25000)
-		var acc2=new account("S124",1002,10000)
-		var acc3=new account("S125",1003,50000)
-		var acc4=new account("S126",1004,-1000)
-		var acc5=new account("S127",1005,-10000)
-		var acc6=new account("S128",1006,12000)
+		var acc1=new account("98745612V",1001,25000)
+		var acc2=new account("95412365V",1002,10000)
+		var acc3=new account("75412368V",1003,50000)
+		var acc4=new account("69854122V",1004,-1000)
+		var acc5=new account("92146385V",1005,-10000)
+		var acc6=new account("85123698V",1006,12000)
 		val bank:List[account]=List(acc1,acc2,acc3,acc4,acc5,acc6)
 
 		print("Overdraft account numbers   : ")
@@ -34,6 +34,6 @@ object Q4
 	}
 
 	val overdraft=(list:List[account])=>list.filter(x=>x.accBalance<=0)
-	val balance=(list:List[account])=>list.reduce((x,y)=>new account("S000",1000,x.accBalance+y.accBalance))
+	val balance=(list:List[account])=>list.reduce((x,y)=>new account("00000V",00001,x.accBalance+y.accBalance))
 	val interest=(list:List[account])=>list.map(x=>(if(x.accBalance>0) x.accBalance*1.05d else x.accBalance*1.1d))
 }
