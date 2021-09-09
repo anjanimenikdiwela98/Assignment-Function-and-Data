707db218acf58d5b5b2bef9622a921a9f96273b4
anjanimenikdiwela98 <62288674+anjanimenikdiwela98@users.noreply.github.com>
2021-09-09 09:46:38 +0530

    Update Q3.scala

--- Q3.scala
+++ Q3.scala
@@ -7,14 +7,14 @@ class account(idNumber:String,accountNumber:Int,accountBalance:Double){
 	def deposit(amount:Double)= new account(this.nicNumber,this.accNumber,this.accBalance+amount)
 	def transfer(Acc:account,amount:Double)={
 	  if(this.accBalance>amount){
-		  val transferAcc = this.withdraw(amount)
-		  val transferedAcc = Acc.deposit(amount)
+		val transferAcc = this.withdraw(amount)
+		val transferedAcc = Acc.deposit(amount)
       
-      println("Transfer=>  "+transferAcc)
-		  println("Transfered=>  "+transferedAcc)
+      		println("Transfer=>  "+transferAcc)
+		println("Transfered=>  "+transferedAcc)
 	  }
-    else{
-		  println("Your account balnce is not sufficient.")
+    	  else{
+		println("Your account balnce is not sufficient.")
 	  }
 	  
   }
@@ -26,8 +26,8 @@ class account(idNumber:String,accountNumber:Int,accountBalance:Double){
 object Q3 extends App{
 		println("\t   Bank Tranfer")
 
-		var acc1 = new account("S123",1000,35000)
-		var acc2 = new account("S124",1001,10000)
+		var acc1 = new account("98745612V",1000,35000)
+		var acc2 = new account("68745125V",1001,12000)
 
 		acc1.transfer(acc2,10000)
 	
